45a9135d3ae40be6f094b6ef1b216439bf94a32d
gebray21 <56612131+gebray21@users.noreply.github.com>
2023-09-03 19:43:56 +0200

    breach 4

---
+++ Unity Projects/Assets/Breach/Player/PlayerSpawn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breach.WildWaters.Player
+{
+    /// <summary>
+    /// Marker component to define a player spawn-point
+    /// </summary>
+    public class PlayerSpawn : MonoBehaviour { } // TODO: Add some custom editor gizmo to this
+
+  
+    }
+
